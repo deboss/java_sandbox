3b61e4d5bb2ac16ae643d2088934cef0003240aa
Tomasz_Debosz <Tomasz_Debosz@epam.com>
2016-05-23 15:08:27 +0200

    some other str testing

--- java_sandbox/src/sandbox/java/lang/TestTheStringReferenceEquality.java
+++ java_sandbox/src/sandbox/java/lang/TestTheStringReferenceEquality.java
@@ -2,14 +2,31 @@ package sandbox.java.lang;
 
 public class TestTheStringReferenceEquality {
 	
-	private static final String TEST1 = "test";
-	private static final String TEST2 = "test";
+	private static final String STR1 = "test";
+	private static final String STR2 = "test";
 	
 	public static void main(String[] args) {
 				
 		areStringVariablesReferencesEqualAndHaveSameHashCode();
 		areStringFinalStaticReferencesEqual();
+		areLongStringsCachedAsWell();
+		whatIsTheHashCodeOfAString();
+		
+		whatHappensDuringConcatenation();
 	}
+
+
+	private static void whatHappensDuringConcatenation() {
+		String result = "" + "" + " " + "";
+	}
+
+
+	private static void whatIsTheHashCodeOfAString() {
+		System.out.println("Hash code of \"\" => " + "".hashCode());
+		System.out.println("Hash code of \"1\" => " + "1".hashCode());
+		System.out.println("Hash code of \"X1\" => " + "X1".hashCode());
+	}
+
 
 	private static void areStringVariablesReferencesEqualAndHaveSameHashCode() {
 		String str1 = "test";
@@ -28,7 +45,14 @@ public class TestTheStringReferenceEquality {
 	}
 
 	private static void areStringFinalStaticReferencesEqual() {
-		System.out.println("TEST1 == TEST2 => " + (TEST1 == TEST2));
-		
+		System.out.println("TEST1 == TEST2 => " + (STR1 == STR2));
+	}
+
+	private static void areLongStringsCachedAsWell() {
+		String str1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+		String str2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+
+		System.out.println("str1 == str2 => " + (str1 == str2));
+		System.out.println("str1.hashCode() => " + str1.hashCode());
 	}
 }
